25ce3b6bf7af154de5ecf7a5d99f33cb9cd796a9
Wirads123 <43898474+Wirads123@users.noreply.github.com>
2018-10-06 10:28:05 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=eaMH4HRyE8A03GOW4lQoEYIE4AFWAnhEW6Jq2RKI";
         
         $("#loadImage").click(function(){
             $.ajax({
